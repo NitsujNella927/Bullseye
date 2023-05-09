b65a6e2e9e49f3c023466416ab7e2fc3bcf2f2a8
NitsujNella927 <nitsujnella@gmail.com>
2023-05-09 13:59:10 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Justin Allen on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
