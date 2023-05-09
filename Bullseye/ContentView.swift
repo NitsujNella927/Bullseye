8c5274b7f4d3a193f343486173cb0540f65598f8
NitsujNella927 <nitsujnella@gmail.com>
2023-05-09 16:40:19 -0400

    add initial layout to app

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,19 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
+      Text("89")
+        .font(.largeTitle)
+        .kerning(-1.0)
+        .fontWeight(.black)
+      HStack {
+        Text("1")
+          .fontWeight(.bold)
+        Slider(value: .constant(50), in:1.0...100.0)
+        Text("100")
+          .fontWeight(.bold)
+      }
+      Button("Hit me") {
+        
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
